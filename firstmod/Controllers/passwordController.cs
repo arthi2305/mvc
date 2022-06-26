f13e1c3af492a07202a0ad9106c1f425b75d7276
arthi2305 <HP@DESKTOP-PR29BJI>
2022-06-26 15:56:02 +0530

    Add project files.

---
+++ firstmod/Controllers/passwordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstmod.Controllers
+{
+    public class passwordController : Controller
+    {
+        //
+        // GET: /password/
+        public ActionResult password()
+        {
+            return View();
+        }
+        
---
+++ firstmod/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(firstmod.Startup))]
+namespace firstmod
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
